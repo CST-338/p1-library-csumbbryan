c44ec86b6d3b6d980f53787bcebd403fb8b3229f
csumbbryan <bzanoli@csumb.edu>
2023-10-31 12:07:05 -0700

    Updated convertInt, convertDate, and addBooks methods.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -16,16 +16,20 @@ public class Library {
   private HashMap<Book, Integer> books;
 
   public Library(String name) {
-    System.out.println("Not implemented");
+    this.name = name;
+    readers = new ArrayList<>();
+    shelves = new HashMap<>();
+    books = new HashMap<>();
+    System.out.println("Barely implemented");
   }
 
   public Code init(String filename) {
     Code code = null; //investigate this
     File file = new File(filename);
     Scanner fileScan;
-    int bookCount = 0;
-    int shelfCount = 0;
-    int readerCount = 0;
+    Integer bookCount;
+    Integer shelfCount;
+    Integer readerCount;
     try {
       fileScan = new Scanner(file);
     } catch (FileNotFoundException e) {
@@ -33,36 +37,35 @@ public class Library {
     }
     //obtain first number from first line
     if(fileScan.hasNext()) {
-      bookCount = convertInt(fileScan.nextLine(), code);
-    }
-    //call initBooks using number from first line, and pass scanner
-    if(bookCount < 0) {
-      //handle code to return proper error code
-
-    } else {
-      initBooks(bookCount, fileScan);
-      listBooks();
-    }
-    if(fileScan.hasNext()) {
-      shelfCount = convertInt(fileScan.nextLine(), code);
-    }
-    if(shelfCount < 0) {
-      //handle code to return proper error code
-    } else {
-      initShelves(shelfCount, fileScan);
-      listShelves();
+      bookCount = convertInt(fileScan.nextLine(), Code.BOOK_COUNT_ERROR);
+      if(bookCount < 0) {
+        return Code.BOOK_COUNT_ERROR;
+      } else {
+        initBooks(bookCount, fileScan);
+        listBooks();
+      }
+    //call initShelves using number from next line, and pass scanner
     }
     if(fileScan.hasNext()) {
-      readerCount = convertInt(fileScan.nextLine(), code);
+      shelfCount = convertInt(fileScan.nextLine(), Code.SHELF_COUNT_ERROR);
+      if(shelfCount < 0) {
+        return Code.SHELF_COUNT_ERROR;
+      } else {
+        initShelves(shelfCount, fileScan);
+        listShelves();
+      }
     }
-    if(readerCount < 0) {
-      //handle code to return proper error code
-    } else {
-      initReader(readerCount, fileScan);
-      listReaders();
+    //call initReader using number from line, and pass scanner
+    if(fileScan.hasNext()) {
+      readerCount = convertInt(fileScan.nextLine(), Code.READER_COUNT_ERROR);
+      if(readerCount < 0) {
+        return Code.READER_COUNT_ERROR;
+      } else {
+        initReader(readerCount, fileScan);
+        listReaders();
+      }
     }
-
-
+    return Code.SUCCESS;
   }
 
   public String getName() {
@@ -71,60 +74,73 @@ public class Library {
 
   private Code initBooks(int bookCount, Scanner scan) {
     Code code = null;
-    List<String> tempString = new ArrayList<>();
-    if(bookCount < 0) {
+      if(bookCount < 0) {
       return Code.LIBRARY_ERROR;
     }
     for(int i = 0; i<bookCount; i++) {
+      List<String> tempString = new ArrayList<>();
       for(String str : scan.nextLine().split(",")) {
         tempString.add(str);
       }
+      if(tempString.size() < Book.DUE_DATE_-1) {
+        return Code.UNKNOWN_ERROR;
+      }
+      //Troubleshooting Begins
+      System.out.println("Adding Book: ISBN: "
+          + tempString.get(Book.ISBN_)
+          + " Title: " + tempString.get(Book.TITLE_)
+          + " Subject: " + tempString.get(Book.SUBJECT_)
+          + " Page Count: " + convertInt(tempString.get(Book.PAGE_COUNT_), Code.BOOK_COUNT_ERROR)
+          + " Author: " + tempString.get(Book.AUTHOR_)
+          + " Due Date: " + convertDate(tempString.get(Book.DUE_DATE_), Code.DUE_DATE_ERROR)
+          );
+      //Troubleshooting Ends
       Book book = new Book(
-          tempString.get(0),
-          tempString.get(1),
-          tempString.get(2),
-          convertInt(tempString.get(3), code),
-          tempString.get(4),
-          convertDate(tempString.get(5), code));
+          tempString.get(Book.ISBN_),
+          tempString.get(Book.TITLE_),
+          tempString.get(Book.SUBJECT_),
+          convertInt(tempString.get(Book.PAGE_COUNT_), Code.BOOK_COUNT_ERROR),
+          tempString.get(Book.AUTHOR_),
+          convertDate(tempString.get(Book.DUE_DATE_), Code.DUE_DATE_ERROR));
       addBook(book);
     }
   return Code.SUCCESS;
   }
 
   private Code initShelves(int shelfCount, Scanner scan) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   private Code initReader(int readerCount, Scanner scan) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Code addBook(Book newBook) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Code returnBook(Reader reader, Book book) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Code returnBook(Book book) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   private Code addBookToShelf(Book book, Shelf shelf) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public int listBooks() {
-
+    return -1; //Not implemented
   }
 
   public Code checkOutBook(Reader reader, Book book) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Book getBookByISBN(String isbn) {
-
+    return null; //Not implemented
   }
 
   public int listShelves() {
@@ -132,43 +148,43 @@ public class Library {
   }
 
   public int listShelves(boolean showbooks) {
-
+    return -1; //Not implemented
   }
 
   public Code addShelf(String shelfSubject) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Code addShelf(Shelf shelf) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Shelf getShelf(Integer shelfNumber) {
-
+    return null; //Not implemented
   }
 
   public Shelf getShelf(String subject) {
-
+    return null; //Not implemented
   }
 
   public int listReaders() {
-
+    return -1; //Not implemented
   }
 
   public int listReaders(boolean showBooks) {
-
+    return -1; //Not implemented
   }
 
   public Reader getReaderByCard(int cardNumber) {
-
+    return null; //Not implemented
   }
 
   public Code addReader(Reader reader) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   public Code removeReader() {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 
   /**
@@ -185,30 +201,62 @@ public class Library {
     } catch (NumberFormatException e) {
       System.out.println("Value which caused the error: " + recordCountString);
       System.out.println("Error message: " + code.getMessage());
+      switch (code) {
+        case BOOK_COUNT_ERROR:
+          System.out.println("Error: Could not read number of books");
+        case PAGE_COUNT_ERROR:
+          System.out.println("Error: could not parse page count");
+        case DATE_CONVERSION_ERROR:
+          System.out.println("Error: Could not parse date component");
+        default:
+          System.out.println("Error: Unknown conversion error");
+      }
       return code.getCode();
-    }
-    switch(code) {
-      case BOOK_COUNT_ERROR:
-        System.out.println("Error: Could not read number of books");
-      case PAGE_COUNT_ERROR:
-        System.out.println("Error: could not parse page count");
-      case DATE_CONVERSION_ERROR:
-        System.out.println("Error: Could not parse date component");
-      default:
-        System.out.println("Error: Unknown conversion error");
     }
     return count;
   }
 
+  //Should the if statements be cleaned up below? Could we call convertDate recursively?
+  //
   static public LocalDate convertDate(String date, Code errorCode) {
-
+    Integer year;
+    Integer month;
+    Integer day;
+    if (date.equals("0000")) {
+      return LocalDate.of(1970, 1, 1);
+    } else {
+      String[] tempStringArray = date.split("-");
+      if(tempStringArray.length != 3) {
+        System.out.println("ERROR: date conversion error, could not parse " + date);
+        System.out.println("Using default date (01-jan-1970");
+        return LocalDate.of(1970,1,1);
+      }
+      year = convertInt(tempStringArray[0], Code.DATE_CONVERSION_ERROR);
+      month = convertInt(tempStringArray[1], Code.DATE_CONVERSION_ERROR);
+      day = convertInt(tempStringArray[2], Code.DATE_CONVERSION_ERROR);
+      if (year < 0) {
+        System.out.println("Error converting date: Year " + year);
+      }
+      if (month < 0) {
+        System.out.println("Error converting date: Month " + month);
+      }
+      if (day < 0) {
+        System.out.println("Error converting date: Day " + day);
+      }
+      if (!(year < 0 && month < 0 && day < 0)) {
+        return LocalDate.of(year, month, day);
+      } else {
+        System.out.println("Using default date (01-jan-1970)");
+        return LocalDate.of(1970, 1, 1);
+      }
+    }
   }
 
   static public int getLibraryCardNumber() {
-
+    return -1; //Not implemented
   }
 
   private Code errorCode(int codeNumber) {
-
+    return Code.NOT_IMPLEMENTED_ERROR;
   }
 }
