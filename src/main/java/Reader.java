e081b1351a87d8ec4116081657e497f8a3539a25
csumbbryan <bzanoli@csumb.edu>
2023-10-30 18:50:06 -0700

    Commit of reader and Shelf with all fields and methods build out.

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,4 +1,122 @@
+import Utilities.Code;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Bryan Zanoli
+ * @since OCtober 30, 2023
+ *
+ * Abstract: ...
+ */
 
 public class Reader {
+  private static final int CARD_NUMBER_ = 0;
+  private static final int NAME_ = 1;
+  private static final int PHONE_ = 2;
+  private static final int BOOK_COUNT_ = 3;
+  private static final int BOOK_START_ = 4;
+
+  private int cardNumber;
+  private String name;
+  private String phone;
+  private List<Book> books;
+
+  public Reader(int cardNumber, String name, String phone) {
+    this.cardNumber = cardNumber;
+    this.name = name;
+    this.phone = phone;
+    books = new ArrayList<>();
+  }
+
+  public Code addBook(Book book) {
+    for (Book compare : books) {
+      if(compare.equals(book)) {
+        return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
+      }
+    }
+    books.add(book);
+    return Code.SUCCESS;
+  }
+
+  public Code removeBook(Book book) {
+    if(books.isEmpty()) {return Code.READER_DOESNT_HAVE_BOOK_ERROR;}
+    for (Book compare : books) {
+      if (compare.equals(book)) {
+        books.remove(book);
+        return Code.SUCCESS;
+      } else {
+        return Code.READER_DOESNT_HAVE_BOOK_ERROR;
+      }
+    }
+    return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
+  }
+
+  public boolean hasBook(Book book) {
+    for (Book compare : books) {
+      if(compare.equals(book)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public int getBookCount() {
+    return books.size();
+  }
+
+  public List<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(List<Book> books) {
+    this.books = books;
+  }
+
+  public int getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(int cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Reader reader = (Reader) o;
+    return cardNumber == reader.cardNumber && Objects.equals(name, reader.name)
+        && Objects.equals(phone, reader.phone);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(cardNumber, name, phone);
+  }
+
+  public String toString() {
+    return name + " (#" + cardNumber + ") " + books;
+  }
 
 }
--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,4 +1,120 @@
+import Utilities.Code;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Shelf {
+  private static final int SHELF_NUMBER_ = 0;
+  private static final int SUBJECT = 1;
+  private int shelfNumber;
+  private String subject;
+  private HashMap<Book, Integer> books;
+
+  public Shelf() {
+    this.shelfNumber = 0;
+    this.subject = "";
+    this.books = new HashMap<>();
+  }
+
+  public Shelf(int shelfNumber, String subject) {
+    this.shelfNumber = shelfNumber;
+    this.subject = subject;
+    this.books = new HashMap<>();
+  }
+
+  public int getShelfNumber() {
+    return shelfNumber;
+  }
+
+  public void setShelfNumber(int shelfNumber) {
+    this.shelfNumber = shelfNumber;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public HashMap<Book, Integer> getBooks() {
+    return books;
+  }
+
+  public void setBooks(HashMap<Book, Integer> books) {
+    this.books = books;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Shelf shelf = (Shelf) o;
+    return shelfNumber == shelf.shelfNumber && Objects.equals(subject, shelf.subject);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(shelfNumber, subject);
+  }
+
+  public String toString() {
+    return shelfNumber + " : " + subject;
+  }
+
+  public int getBookCount(Book book) {
+    return this.books.containsKey(book) ? this.books.get(book) : -1;
+  }
+
+  public Code addBook(Book book) {
+    for(Book hashBook : books.keySet()) {
+      if(hashBook.equals(book)) {
+        int count = books.get(book);
+        books.replace(book, ++count);
+        System.out.println(book.toString() + this.toString());
+        return Code.SUCCESS;
+      }
+    }
+    if(!books.containsKey(book) && book.subject.equals(this.subject)) {
+      books.put(book, 1);
+      System.out.println(book.toString() + this.toString());
+      return Code.SUCCESS;
+    }
+    return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+  }
+
+  public Code removeBook(Book book) {
+    if(books.containsKey(book)) {
+      if(books.get(book) == 0) {
+        System.out.println(book.title + " is not on shelf " + this.subject);
+        return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+      } else {
+        int count = books.get(book);
+        books.put(book, --count);
+        return Code.SUCCESS;
+      }
+
+    } else {
+      System.out.println("No copies of " + book.title + " remain on shelf " + this.subject);
+      return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+    }
+  }
+
+  public String listBooks() {
+    StringBuilder bookList = new StringBuilder(new String());
+    for(Book book : this.books.keySet()) {
+      bookList.append(book.title).append(" by ").append(book.author).append(" ISBN:")
+          .append(book.isbn).append(" ").append(this.books.get(book)).append("\n");
+    }
+    return this.books.keySet().size()
+        + " book on shelf: "
+        + shelfNumber
+        + " : " + this.subject
+        + "\n" + bookList;
+  }
 
 }
