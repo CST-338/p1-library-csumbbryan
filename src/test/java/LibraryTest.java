7a2d6cdb4d459e00ed74d33ee5fc6606ed33781d
csumbbryan <bzanoli@csumb.edu>
2023-11-06 20:34:13 -0800

    Updated Reader.java javadocs.

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,180 @@
+import Utilities.Code;
+import java.time.LocalDate;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+
+    Library csumb = null;
+
+    String library00 = "Library00.csv";
+    String library01 = "Library01.csv";
+    String badBooks0 = "badBooks0.csv";
+    String badBooks1 = "badBooks1.csv";
+    String badShelves0 = "badShelves0.csv";
+    String badShelves1 = "badShelves1.csv";
+    String badReader0 = "badReader0.csv";
+    String badReader1 = "badReader1.csv";
+
+    @BeforeEach
+    void setUp() {
+        csumb = new Library("CSUMB");
+    }
+
+    @AfterEach
+    void tearDown() {
+        csumb = null;
+    }
+
+    @Test
+    void init_test() {
+        //Bad file
+        assertEquals(Code.FILE_NOT_FOUND_ERROR, csumb.init("nope.csv"));
+
+        //Test that file cannot read book count -- has value of cat instead of 3
+        assertEquals(Code.BOOK_COUNT_ERROR, csumb.init(badBooks0));
+
+        //Test that
+        assertEquals(Code.UNKNOWN_ERROR, csumb.init(badBooks1)); //This was Shelf Count Error
+        assertEquals(Code.SHELF_COUNT_ERROR, csumb.init(badShelves0));
+
+        //Test that badShelves1 fails due to incorrect Shelf values
+        assertEquals(Code.SHELF_NUMBER_PARSE_ERROR, csumb.init(badShelves1)); //This was Reader Count Error
+
+        //Test that badReader0 fails due to incorrect reader count value
+        assertEquals(Code.READER_COUNT_ERROR, csumb.init(badReader0));
+
+        //Test that Drew Clinkenbeard books cannot be read in but init otherwise succeeds:
+        assertEquals(Code.SUCCESS, csumb.init(badReader1)); //Is this the right error?
+        assertEquals(0, csumb.getReaderByCard(1).getBookCount());
+
+    }
+
+    @Test
+    void init_goodFile_test() {
+        assertEquals(Code.SUCCESS, csumb.init(library00));
+
+        //Need to create new library to test second init
+        csumb = null;
+        csumb = new Library("CSUMB");
+        assertEquals(Code.SUCCESS, csumb.init(library01)); //Can't initialize twice in same Lib
+    }
+
+    /**
+     * Tests conditions in which a book may be added. Including no shelf exists,
+     * book already exists, and new book added.
+     */
+    @Test
+    void addBook() {
+        //Before Test Setup
+        Book theArena = new Book(
+            "0101",
+            "The Arena",
+            "fantasy",
+            300,
+            "Robert Smith",
+            LocalDate.now()
+        );
+        Book chemistry = new Book(
+            "h2o",
+            "Chemistry",
+            "education",
+            450,
+            "Arthur Nobel",
+            LocalDate.now()
+        );
+        String fantasy = "fantasy";
+        //Conditions:
+        //Shelf doesn't exist
+        assertEquals(Code.SHELF_EXISTS_ERROR,csumb.addBook(theArena));
+
+        //Book does not exist and count set to 1
+        csumb.init(library00);
+        assertEquals(Code.SUCCESS, csumb.addBook(chemistry));
+        assertEquals(1, csumb.getShelf("education").getBookCount(chemistry));
+
+        //Book exists already and book count increased by 1
+        assertEquals(Code.SUCCESS, csumb.addShelf(fantasy));
+        csumb.addBook(theArena);
+        assertEquals(2, csumb.getShelf(fantasy).getBookCount(theArena));
+    }
+
+    @Test
+    void returnBook() {
+    }
+
+    @Test
+    void testReturnBook() {
+    }
+
+    @Test
+    void listBooks() {
+    }
+
+    @Test
+    void checkOutBook() {
+    }
+
+    @Test
+    void getBookByISBN() {
+    }
+
+    @Test
+    void listShelves() {
+    }
+
+    @Test
+    void addShelf() {
+    }
+
+    @Test
+    void testAddShelf() {
+    }
+
+    @Test
+    void getShelf() {
+        //BE SURE TO TEST getShelf WITH NON-EXISTENT SHELF
+        Shelf shelf = new Shelf(1, "fantasy");
+        assertNull(csumb.getShelf(shelf.getSubject()));
+        assertEquals(Code.SUCCESS, csumb.addShelf(shelf));
+    }
+
+    @Test
+    void testGetShelf() {
+    }
+
+    @Test
+    void listReaders() {
+    }
+
+    @Test
+    void testListReaders() {
+    }
+
+    @Test
+    void getReaderByCard() {
+    }
+
+    @Test
+    void addReader() {
+    }
+
+    @Test
+    void removeReader() {
+    }
+
+    @Test
+    void convertInt() {
+    }
+
+    @Test
+    void convertDate() {
+    }
+
+    @Test
+    void getLibraryCardNumber() {
+    }
+}
